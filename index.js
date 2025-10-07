ad38c823446d97f8d1d6c39473724bb7ba648cc0
AhnizDigital <ahnizdigital@gmail.com>
2025-10-07 14:48:03 +0100

    gitignore passed version 1.0.1

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const port = 3000;
 app.get("/", (req, res) => {
   let name = superheroes.random();
   res.send(`<h1>Behold, I am ${name}!</h1>`);
+  console.log(`Generated superhero name: ${name}`);
 });
 
 app.listen(port, () => {
